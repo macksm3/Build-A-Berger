c5c910ba2f6aad1db01ca4bd6047d549d6107e5f
MacksM3 <64023042+macksm3@users.noreply.github.com>
2020-09-16 16:59:33 -0400

    add style

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Sauerkraut and Cale', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Texas Barbecue', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Peanut Butter', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spam Burger with spam, eggs, sausage and spam', false);
